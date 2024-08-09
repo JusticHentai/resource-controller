22672143914019e4b5656ef5d28d75c171dcba5d
JusticHentai <1360636180@qq.com>
2024-08-09 10:46:48 +0800

    feat: 插队

--- src/utils/loadCurrent.ts
+++ src/utils/loadCurrent.ts
@@ -5,19 +5,8 @@ const loadCurrent = async (loadQueue: LoadQueue, resourceMap: ResourceMap) => {
   const currentLoadList = loadList[0]
   const priorityList = loadQueue.priorityList
 
-  const test = []
-  for (const item of loadQueue.loadList) {
-    test.push([...item])
-  }
-
-  console.log('=== 每次执行 loadCurrent ===', {
-    loadList: test,
-    priorityList: [...loadQueue.priorityList],
-  })
-
   // 所有队列处理完毕
   if (!loadList.length) {
-    console.log('=== loadCurrent 整体跑完 ===', loadQueue)
     return
   }
 
@@ -25,8 +14,6 @@ const loadCurrent = async (loadQueue: LoadQueue, resourceMap: ResourceMap) => {
   if (!currentLoadList.length) {
     priorityList?.shift()
     loadList.shift()
-
-    console.log('=== loadCurrent 删除 ===')
 
     await loadCurrent(loadQueue, resourceMap)
 
